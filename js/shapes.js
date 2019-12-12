0e7c42d1fa551d3f44fb585bd14d6fae4ca3facd
mikeyshriner <shrinermichael@gmail.com>
2019-12-11 23:06:36 -0500

    smile

--- js/shapes.js
+++ js/shapes.js
@@ -248,7 +248,7 @@ switch (color) {
    ctx.arc(512 + 0.4 * radius, 256 - 0.4* radius, radius_eyes, 0, 2 * Math.PI);
    ctx.stroke();
    ctx.closePath();
-};
+}; 
 
 /*
  * Exercise 6 (extra credit).
